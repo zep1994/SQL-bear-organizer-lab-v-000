7a0590030b8086471deb3d632b011e7aa3cf2637
Thomas Wesley Matlock <thomasmatlock1994@gmail.com>
2018-04-04 13:36:20 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -15,7 +15,7 @@ def selects_oldest_bear_and_returns_name_and_age
 end
 
 def select_youngest_bear_and_returns_name_and_age
-  ""
+  "SELECT bears.name, bears.age FROM bears ORDER BY age LIMIT 1"
 end
 
 def selects_most_prominent_color_and_returns_with_count
